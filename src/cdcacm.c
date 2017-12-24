82c12ad5687bd4e290ca0bcba6d3a8685be4d767
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-23 23:39:06 -0800

    Add a third USB CDC ACM port, bridged to USART1 (PA9/PA10)

--- src/cdcacm.c
+++ src/cdcacm.c
@@ -59,6 +59,15 @@ static const struct usb_device_descriptor dev = {
 };
 
 /* Serial ACM interfaces */
+static const struct usb_endpoint_descriptor uart_comm_endp1[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x86,
+	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+	.wMaxPacketSize = 16,
+	.bInterval = 255,
+}};
+
 static const struct usb_endpoint_descriptor uart_comm_endp2[] = {{
 	.bLength = USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType = USB_DT_ENDPOINT,
@@ -75,6 +84,22 @@ static const struct usb_endpoint_descriptor uart_comm_endp3[] = {{
 	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
 	.wMaxPacketSize = 16,
 	.bInterval = 255,
+}};
+
+static const struct usb_endpoint_descriptor uart_data_endp1[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x05,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = CDCACM_PACKET_SIZE,
+	.bInterval = 1,
+}, {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x85,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = CDCACM_PACKET_SIZE,
+	.bInterval = 1,
 }};
 
 static const struct usb_endpoint_descriptor uart_data_endp2[] = {{
@@ -108,6 +133,42 @@ static const struct usb_endpoint_descriptor uart_data_endp3[] = {{
 	.wMaxPacketSize = CDCACM_PACKET_SIZE,
 	.bInterval = 1,
 }};
+
+static const struct {
+	struct usb_cdc_header_descriptor header;
+	struct usb_cdc_call_management_descriptor call_mgmt;
+	struct usb_cdc_acm_descriptor acm;
+	struct usb_cdc_union_descriptor cdc_union;
+} __attribute__((packed)) uart_cdcacm_functional_descriptors1 = {
+	.header = {
+		.bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_HEADER,
+		.bcdCDC = 0x0110,
+	},
+	.call_mgmt = {
+		.bFunctionLength =
+			sizeof(struct usb_cdc_call_management_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
+		.bmCapabilities = 0,
+		.bDataInterface = 5,
+	},
+	.acm = {
+		.bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_ACM,
+		.bmCapabilities = 2, /* SET_LINE_CODING supported*/
+	},
+	.cdc_union = {
+		.bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_UNION,
+		.bControlInterface = 4,
+		.bSubordinateInterface0 = 5,
+	 }
+};
+
 
 static const struct {
 	struct usb_cdc_header_descriptor header;
@@ -179,6 +240,24 @@ static const struct {
 	 }
 };
 
+static const struct usb_interface_descriptor uart_comm_iface1[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 4,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 1,
+	.bInterfaceClass = USB_CLASS_CDC,
+	.bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
+	.bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
+	.iInterface = 5,
+
+	.endpoint = uart_comm_endp1,
+
+	.extra = &uart_cdcacm_functional_descriptors1,
+	.extralen = sizeof(uart_cdcacm_functional_descriptors1)
+}};
+
+
 static const struct usb_interface_descriptor uart_comm_iface2[] = {{
 	.bLength = USB_DT_INTERFACE_SIZE,
 	.bDescriptorType = USB_DT_INTERFACE,
@@ -215,6 +294,21 @@ static const struct usb_interface_descriptor uart_comm_iface3[] = {{
 	.extralen = sizeof(uart_cdcacm_functional_descriptors3)
 }};
 
+static const struct usb_interface_descriptor uart_data_iface1[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 5,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_DATA,
+	.bInterfaceSubClass = 0,
+	.bInterfaceProtocol = 0,
+	.iInterface = 0,
+
+	.endpoint = uart_data_endp1,
+}};
+
+
 static const struct usb_interface_descriptor uart_data_iface2[] = {{
 	.bLength = USB_DT_INTERFACE_SIZE,
 	.bDescriptorType = USB_DT_INTERFACE,
@@ -243,6 +337,17 @@ static const struct usb_interface_descriptor uart_data_iface3[] = {{
 
 	.endpoint = uart_data_endp3,
 }};
+
+static const struct usb_iface_assoc_descriptor uart_assoc1 = {
+	.bLength = USB_DT_INTERFACE_ASSOCIATION_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE_ASSOCIATION,
+	.bFirstInterface = 4,
+	.bInterfaceCount = 2,
+	.bFunctionClass = USB_CLASS_CDC,
+	.bFunctionSubClass = USB_CDC_SUBCLASS_ACM,
+	.bFunctionProtocol = USB_CDC_PROTOCOL_AT,
+	.iFunction = 0,
+};
 
 static const struct usb_iface_assoc_descriptor uart_assoc2 = {
 	.bLength = USB_DT_INTERFACE_ASSOCIATION_SIZE,
@@ -281,6 +386,13 @@ static const struct usb_interface ifaces[] = {{
 }, {
 	.num_altsetting = 1,
 	.altsetting = uart_data_iface3,
+}, {
+	.num_altsetting = 1,
+	.iface_assoc = &uart_assoc1,
+	.altsetting = uart_comm_iface1,
+}, {
+	.num_altsetting = 1,
+	.altsetting = uart_data_iface1,
 }};
 
 static const struct usb_config_descriptor config = {
@@ -331,6 +443,9 @@ static int cdcacm_control_request(usbd_device *dev,
 		case 2:
 			usbuart_set_line_coding((struct usb_cdc_line_coding*)*buf, USART3);
 			return 1;
+		case 4:
+			usbuart_set_line_coding((struct usb_cdc_line_coding*)*buf, USART1);
+			return 1;
 		default:
 			return 0;
 		}
@@ -375,6 +490,13 @@ static void cdcacm_set_config(usbd_device *dev, uint16_t wValue)
 	              CDCACM_PACKET_SIZE, usbuart_usb_in_cb);
 	usbd_ep_setup(dev, 0x84, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
+	usbd_ep_setup(dev, 0x05, USB_ENDPOINT_ATTR_BULK,
+	              CDCACM_PACKET_SIZE, usbuart1_usb_out_cb);
+	usbd_ep_setup(dev, 0x85, USB_ENDPOINT_ATTR_BULK,
+	              CDCACM_PACKET_SIZE, usbuart_usb_in_cb);
+	usbd_ep_setup(dev, 0x86, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+
+
 	usbd_register_control_callback(dev,
 			USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
 			USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
@@ -385,6 +507,7 @@ static void cdcacm_set_config(usbd_device *dev, uint16_t wValue)
 	 */
 	cdcacm_set_modem_state(dev, 0, true, true);
 	cdcacm_set_modem_state(dev, 2, true, true);
+	cdcacm_set_modem_state(dev, 4, true, true);
 }
 
 /* We need a special large control buffer for this device: */
